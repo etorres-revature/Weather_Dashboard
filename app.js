64ad4e28785317cc2c99f75d8edeecd20ee7ee04
Eric D. Torres <etorresnotary@gmail.com>
2020-08-23 11:27:52 -0500

    updated js with api calls, base URLS, and variables

--- app.js
+++ app.js
@@ -1,83 +1,9 @@
-const apiKey = "1Y1w9yctnCuWpZljCPIwpQAXB3CLCw7z"
-let searchTerms = "";
-let numResults = 0;
-let beginDate = "1851";
-let endDate = "2020";
-let queryURLBase = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}`;
+//openweather api call
+//https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=340e329562e29bd2ff2b681d0bf2d492
 
-var articleCounter = 0;
+//opencage api call to reverse geocode
+//https://api.opencagedata.com/geocode/v1/json?q=chicago%2C%20IL&key=ef6e5295fc1b4624b73a959b2fee725e&language=en&pretty=1
 
-$("#search").on("click", function () {
-    event.preventDefault();
-
-    $(".articles").empty();
-
-    searchTerms = $("#inputSearchTerms").val().trim();
-    // console.log(searchTerms);
-    let searchURL = queryURLBase + "&q=" + searchTerms;
-    // console.log(searchURL);
-    beginDate = $("#inputStartYear").val().trim();
-    endDate = $("#inputEndYear").val().trim();
-
-    if (parseInt(beginDate)) {
-        searchURL = queryURLBase + "&begin_date=" + beginDate + "0101";
-        console.log(searchURL);
-    }
-
-    if (parseInt(endDate)) {
-        searchURL = queryURLBase + "&end_date=" + endDate + "1231";
-        // console.log(searchURL);
-    }
-
-    numResults = $("#inputNumber").val();
-    console.log(numResults);
-
-    $.ajax({
-        url: searchURL,
-        method: "GET"
-    })
-
-        .then(function (dataFromNYT) {
-            // console.log(dataFromNYT.response.docs[0].abstract);
-            // console.log(dataFromNYT.response.docs[0].web_url);
-            // console.log(dataFromNYT.response.docs[0].headline.main);
-            // let results = response.docs;
-            // console.log(results);
-
-            for (var i = 1; i <= numResults; i++) {
-                // console.log("in the for loop")
-                var articleDiv = $("<div>").addClass("card-body").attr("id", "card-body-" + i).css("list-style-type", "none");
-
-                var headline = dataFromNYT.response.docs[i].headline.main;
-                var abstract = dataFromNYT.response.docs[i].abstract;
-                var webURL = dataFromNYT.response.docs[i].web_url;
-                var pHeadline = $("<h4>").text(headline);
-                var aURL = $("<a>").attr("href", webURL).text(webURL)
-                var pAbstract = $("<p>").text(abstract);
-
-                // if (headline === null) {
-                //     pheadline = $("<hr>").text("No headline available");
-                // }
-
-                articleDiv.append(pHeadline);
-                articleDiv.append(aURL);
-                articleDiv.append(pAbstract);
-                articleDiv.append("<hr>");
-
-                $(".articles").append(articleDiv);
-            }
-
-            $("#inputSearchTerms").val("");
-            $("#inputStartYear").val("");
-            $("#inputEndYear").val("");
-
-        });
-});
-
-$("#clear").on("click", function(){
-event.preventDefault();
-$(".articles").empty();
-// $("#inputSearchTerms").val("");
-// $("#inputStartYear").val("");
-// $("#inputEndYear").val("");
-})
+const baseWeatherURL = `https://api.openweathermap.org/data/2.5/onecall?appid=340e329562e29bd2ff2b681d0bf2d492`;
+const baseGeoCodeURL = `https://api.opencagedata.com/geocode/v1/json?key=ef6e5295fc1b4624b73a959b2fee725e&language=en&pretty=1`;
+var
